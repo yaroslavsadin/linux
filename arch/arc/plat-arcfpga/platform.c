ab654776296174e09a8eb60b9e3053995924959f
Vineet Gupta <vgupta@synopsys.com>
2012-08-23 16:49:38 +0530

    ARC: [plat-arfpga] BVCI Latency Unit setup
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/plat-arcfpga/platform.c
+++ arch/arc/plat-arcfpga/platform.c
@@ -17,6 +17,60 @@
 #include <asm/irq.h>
 #include <plat/memmap.h>
 
+/*-----------------------BVCI Latency Unit -----------------------------*/
+
+#ifdef CONFIG_ARC_HAS_BVCI_LAT_UNIT
+
+int lat_cycles = CONFIG_BVCI_LAT_CYCLES;
+
+/* BVCI Bus Profiler: Latency Unit */
+static void __init setup_bvci_lat_unit(void)
+{
+#define MAX_BVCI_UNITS 12
+
+	/* TBD: rewrite this using I/O macros */
+	volatile unsigned int *base = (unsigned int *)BVCI_LAT_UNIT_BASE;
+	volatile unsigned int *lat_unit = (unsigned int *)base + 21;
+	volatile unsigned int *lat_val = (unsigned int *)base + 22;
+	unsigned int unit;
+	const unsigned long units_req = CONFIG_BVCI_LAT_UNITS;
+
+	/*
+	 * There are multiple Latency Units corresponding to the many
+	 * interfaces of the system bus arbiter (both CPU side as well as
+	 * the peripheral side).
+	 *
+	 * Unit  0 - System Arb and Mem Controller - adds latency to all
+	 *	    memory trasactions
+	 * Unit  1 - I$ and System Bus
+	 * Unit  2 - D$ and System Bus
+	 * ..
+	 * Unit 12 - IDE Disk controller and System Bus
+	 *
+	 * The programmers model requires writing to lat_unit reg first
+	 * and then the latency value (cycles) to lat_value reg
+	 */
+
+	if (CONFIG_BVCI_LAT_UNITS == 0) {
+		*lat_unit = 0;
+		*lat_val = lat_cycles;
+		pr_info("BVCI Latency for all Memory Transactions %d cycles\n",
+			lat_cycles);
+	} else {
+		for_each_set_bit(unit, &units_req, MAX_BVCI_UNITS) {
+			*lat_unit = unit + 1;  /* above returns 0 based */
+			*lat_val = lat_cycles;
+			pr_info("BVCI Latency for Unit[%d] = %d cycles\n",
+				(unit + 1), lat_cycles);
+		}
+	}
+}
+#else
+static void __init setup_bvci_lat_unit(void)
+{
+}
+#endif
+
 /*----------------------- Platform Devices -----------------------------*/
 
 #if defined(CONFIG_SERIAL_ARC) || defined(CONFIG_SERIAL_ARC_MODULE)
@@ -110,6 +164,8 @@ static void arc_fpga_serial_init(void)
 void __init arc_platform_early_init(void)
 {
 	pr_info("[plat-arcfpga]: registering early dev resources\n");
+
+	setup_bvci_lat_unit();
 
 	arc_fpga_serial_init();
 }
