e8a7d2f3ba7aee2987791755b59b59dd70de7134
Mischa Jonker <mjonker@synopsys.com>
2013-07-10 16:03:45 +0200

    ARC: Make arc_unwind_core accessible externally
    The arc unwinder can also be used for perf callchains.
    
    Signed-off-by: Mischa Jonker <mjonker@synopsys.com>

---
+++ arch/arc/include/asm/stacktrace.h
@@ -0,0 +1,10 @@
+#ifndef __ASM_STACKTRACE_H
+#define __ASM_STACKTRACE_H
+
+#include <linux/sched.h>
+
+notrace noinline unsigned int
+arc_unwind_core(struct task_struct *tsk, struct pt_regs *regs,
+		int (*consumer_fn) (unsigned int, void *), void *arg);
+
+#endif /* __ASM_STACKTRACE_H */
