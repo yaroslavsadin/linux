516afd30b52e353ece693e2057f910c953cc5dbd
Mischa Jonker <mjonker@synopsys.com>
2013-08-26 16:19:14 +0200

    USB changes

--- drivers/usb/host/ehci-platform.c
+++ drivers/usb/host/ehci-platform.c
@@ -206,6 +206,7 @@ static int ehci_platform_resume(struct device *dev)
 static const struct of_device_id vt8500_ehci_ids[] = {
 	{ .compatible = "via,vt8500-ehci", },
 	{ .compatible = "wm,prizm-ehci", },
+	{ .compatible = "snps,h20apb-ehci", },
 	{}
 };
 
