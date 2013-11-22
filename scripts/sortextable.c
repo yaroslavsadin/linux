effbf218764ac3ebb80f57195030204979b7616e
Vineet Gupta <vgupta@synopsys.com>
2013-11-22 13:05:58 +0530

    ARCv2: extable: Enable sorting at build time
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- scripts/sortextable.c
+++ scripts/sortextable.c
@@ -39,6 +39,10 @@
 #define EM_AARCH64	183
 #endif
 
+#ifndef EM_ARCV2
+#define EM_ARCV2	195
+#endif
+
 static int fd_map;	/* File descriptor for file being modified. */
 static int mmap_failed; /* Boolean flag. */
 static void *ehdr_curr; /* current ElfXX_Ehdr *  for resource cleanup */
@@ -273,6 +277,7 @@ do_file(char const *const fname)
 		custom_sort = sort_relative_table;
 		break;
 	case EM_ARCOMPACT:
+	case EM_ARCV2:
 	case EM_ARM:
 	case EM_AARCH64:
 	case EM_MIPS:
