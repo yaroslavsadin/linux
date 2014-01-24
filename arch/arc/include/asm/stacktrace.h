dadf5a70593a4dac3a5e172b9c52fb50ec91f2c3
Mischa Jonker <mjonker@synopsys.com>
2014-01-24 09:56:38 +0100

    ARC: stack unwinder: allow user-space addresses if desired
    Signed-off-by: Mischa Jonker <mjonker@synopsys.com>

--- arch/arc/include/asm/stacktrace.h
+++ arch/arc/include/asm/stacktrace.h
@@ -5,6 +5,7 @@
 
 notrace noinline unsigned int
 arc_unwind_core(struct task_struct *tsk, struct pt_regs *regs,
-		int (*consumer_fn) (unsigned int, void *), void *arg);
+		int (*consumer_fn) (unsigned int, void *), void *arg,
+		bool user_ok);
 
 #endif /* __ASM_STACKTRACE_H */
