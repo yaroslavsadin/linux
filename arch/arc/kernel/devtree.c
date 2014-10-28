32fad94b68f4e2461a65367f018cbb0b93b78e31
Vineet Gupta <vgupta@synopsys.com>
2014-10-28 15:04:05 +0530

    ARC: runtime determine BASE_BAUD per machine
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/devtree.c
+++ arch/arc/kernel/devtree.c
@@ -17,6 +17,26 @@
 #include <asm/clk.h>
 #include <asm/mach_desc.h>
 
+#ifdef CONFIG_SERIAL_8250_CONSOLE
+
+static unsigned int arc_base_baud = 33333333;
+
+unsigned int __init serial8250_early_base_baud(char *options)
+{
+	return arc_base_baud/16;
+}
+
+static void __init arc_set_early_base_baud(unsigned long dt_root)
+{
+	if (of_flat_dt_is_compatible(dt_root, "snps,axs103"))
+		arc_base_baud = 33333333;
+	else if (of_flat_dt_is_compatible(dt_root, "snps,zebu-hs"))
+		arc_base_baud = 50000000;
+}
+#else
+#define arc_set_early_base_baud(dt_root)
+#endif
+
 static const void * __init arch_get_next_mach(const char *const **match)
 {
 	static const struct machine_desc *mdesc = __arch_info_begin;
@@ -55,6 +75,8 @@ const struct machine_desc * __init setup_machine_fdt(void *dt)
 	clk = of_get_flat_dt_prop(dt_root, "clock-frequency", &len);
 	if (clk)
 		arc_set_core_freq(of_read_ulong(clk, len/4));
+
+	arc_set_early_base_baud(dt_root);
 
 	return mdesc;
 }
