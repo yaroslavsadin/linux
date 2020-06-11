227d3b3460659a8ae0fc6ab608e75c67e296cf14
Vineet Gupta <vgupta@synopsys.com>
2020-06-10 23:44:59 -0700

    ARC: boot log: eliminate struct cpuinfo_arc #2: cache
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/setup.h
+++ arch/arc/include/asm/setup.h
@@ -43,6 +43,5 @@ extern char *arc_mmu_mumbojumbo(int cpu_id, char *buf, int len);
 
 extern void arc_cache_init(void);
 extern char *arc_cache_mumbojumbo(int cpu_id, char *buf, int len);
-extern void read_decode_cache_bcr(void);
 
 #endif /* __ASMARC_SETUP_H */
