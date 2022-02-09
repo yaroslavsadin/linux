964fbcea4f1e5629a49ea9f1c19266b98a4f8ac1
Vineet Gupta <vgupta@synopsys.com>
2022-02-09 18:14:36 +0300

    ARC: int vs long
    - arch specific bitsperlong.h
    - general LP64 related changes
    - don't force cast argument to __builtin_arc_sr()
    - irqflags: STAT32 is 32-bits but generic raw_local_irq_* expect flags to be long
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/module.c
+++ arch/arc/kernel/module.c
@@ -42,16 +42,16 @@ void module_arch_cleanup(struct module *mod)
 #endif
 }
 
-int apply_relocate_add(Elf32_Shdr *sechdrs,
+int apply_relocate_add(Elf_Shdr *sechdrs,
 		       const char *strtab,
 		       unsigned int symindex,	/* sec index for sym tbl */
 		       unsigned int relsec,	/* sec index for relo sec */
 		       struct module *module)
 {
 	int i, n, relo_type;
-	Elf32_Rela *rel_entry = (void *)sechdrs[relsec].sh_addr;
-	Elf32_Sym *sym_entry, *sym_sec;
-	Elf32_Addr relocation, location, tgt_addr;
+	Elf_Rela *rel_entry = (void *)sechdrs[relsec].sh_addr;
+	Elf_Sym *sym_entry, *sym_sec;
+	Elf_Addr relocation, location, tgt_addr;
 	unsigned int tgtsec;
 
 	/*
@@ -60,10 +60,10 @@ int apply_relocate_add(Elf32_Shdr *sechdrs,
 	 */
 	tgtsec = sechdrs[relsec].sh_info;
 	tgt_addr = sechdrs[tgtsec].sh_addr;
-	sym_sec = (Elf32_Sym *) sechdrs[symindex].sh_addr;
+	sym_sec = (Elf_Sym *) sechdrs[symindex].sh_addr;
 	n = sechdrs[relsec].sh_size / sizeof(*rel_entry);
 
-	pr_debug("\nSection to fixup %s @%x\n",
+	pr_debug("\nSection to fixup %s @%llx\n",
 		 module->arch.secstr + sechdrs[tgtsec].sh_name, tgt_addr);
 	pr_debug("=========================================================\n");
 	pr_debug("r_off\tr_add\tst_value ADDRESS  VALUE\n");
@@ -78,7 +78,7 @@ int apply_relocate_add(Elf32_Shdr *sechdrs,
 
 		/* This is the symbol it is referring to.  Note that all
 		   undefined symbols have been resolved.  */
-		sym_entry = sym_sec + ELF32_R_SYM(rel_entry[i].r_info);
+		sym_entry = sym_sec + ELF_R_SYM(rel_entry[i].r_info);
 
 		relocation = sym_entry->st_value + rel_entry[i].r_addend;
 
@@ -88,7 +88,7 @@ int apply_relocate_add(Elf32_Shdr *sechdrs,
 			s = strtab + sym_entry->st_name;
 		}
 
-		pr_debug("   %x\t%x\t%x %x %x [%s]\n",
+		pr_debug("   %llx\t%llx\t%llx %llx %llx [%s]\n",
 			 rel_entry[i].r_offset, rel_entry[i].r_addend,
 			 sym_entry->st_value, location, relocation, s);
 
@@ -97,14 +97,14 @@ int apply_relocate_add(Elf32_Shdr *sechdrs,
 		 * global data access again is abs 32 bit.
 		 * Both of these are handled by same relocation type
 		 */
-		relo_type = ELF32_R_TYPE(rel_entry[i].r_info);
+		relo_type = ELF_R_TYPE(rel_entry[i].r_info);
 
 		if (likely(R_ARC_32_ME == relo_type))	/* ME ( S + A ) */
 			arc_write_me((unsigned short *)location, relocation);
 		else if (R_ARC_32 == relo_type)		/* ( S + A ) */
-			*((Elf32_Addr *) location) = relocation;
+			*((Elf_Addr *) location) = relocation;
 		else if (R_ARC_32_PCREL == relo_type)	/* ( S + A ) - PDATA ) */
-			*((Elf32_Addr *) location) = relocation - location;
+			*((Elf_Addr *) location) = relocation - location;
 		else
 			goto relo_err;
 
@@ -118,8 +118,8 @@ int apply_relocate_add(Elf32_Shdr *sechdrs,
 	return 0;
 
 relo_err:
-	pr_err("%s: unknown relocation: %u\n",
-		module->name, ELF32_R_TYPE(rel_entry[i].r_info));
+	pr_err("%s: unknown relocation: %llu\n",
+		module->name, ELF_R_TYPE(rel_entry[i].r_info));
 	return -ENOEXEC;
 
 }
@@ -129,7 +129,7 @@ relo_err:
  * This couldn't be done in module_frob_arch_sections() because
  * relocations had not been applied by then
  */
-int module_finalize(const Elf32_Ehdr *hdr, const Elf_Shdr *sechdrs,
+int module_finalize(const Elf_Ehdr *hdr, const Elf_Shdr *sechdrs,
 		    struct module *mod)
 {
 #ifdef CONFIG_ARC_DW2_UNWIND
--- arch/arc/kernel/ptrace.c
+++ arch/arc/kernel/ptrace.c
@@ -9,6 +9,7 @@
 #include <linux/regset.h>
 #include <linux/unistd.h>
 #include <linux/elf.h>
+#include <asm/asm-offsets.h>
 
 static struct callee_regs *task_callee_regs(struct task_struct *tsk)
 {
@@ -106,13 +107,13 @@ static int genregs_set(struct task_struct *target,
 		ret = user_regset_copyin(&pos, &count, &kbuf, &ubuf, \
 			(void *)(PTR), \
 			offsetof(struct user_regs_struct, LOC), \
-			offsetof(struct user_regs_struct, LOC) + 4);
+			offsetof(struct user_regs_struct, LOC) + REGSZ);
 
 #define REG_IGNORE_ONE(LOC)		\
 	if (!ret)			\
 		ret = user_regset_copyin_ignore(&pos, &count, &kbuf, &ubuf, \
 			offsetof(struct user_regs_struct, LOC), \
-			offsetof(struct user_regs_struct, LOC) + 4);
+			offsetof(struct user_regs_struct, LOC) + REGSZ);
 
 	REG_IGNORE_ONE(pad);
 
@@ -205,7 +206,7 @@ static int arcv2regs_set(struct task_struct *target,
 	if (IS_ENABLED(CONFIG_ARC_HAS_ACCL_REGS))
 		copy_sz = sizeof(struct user_regs_arcv2);
 	else
-		copy_sz = 4;	/* r30 only */
+		copy_sz = REGSZ;	/* r30 only */
 
 	ret = user_regset_copyin(&pos, &count, &kbuf, &ubuf, (void *)&regs->r30,
 				  0, copy_sz);
@@ -280,7 +281,7 @@ long arch_ptrace(struct task_struct *child, long request,
 asmlinkage int syscall_trace_enter(struct pt_regs *regs)
 {
 	if (tracehook_report_syscall_entry(regs))
-		return ULONG_MAX;
+		return -1;
 
 	return regs->r8;
 }
