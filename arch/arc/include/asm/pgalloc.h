79677cc04dd2cc07bbd6a5d06dd4fd8852871fa3
Vineet Gupta <vgupta@synopsys.com>
2022-02-10 17:35:33 +0300

    ARC: mm: introduce _PAGE_TABLE to explicitly link pgd,pud,pmd entries
    ARCv3 hardware walker expects Table Descriptors to have b'11 in LSB bits
    to continue moving to next level.
    
    This commits adds that (to ARCv2 code) and ensures that it works in
    software walked regime.
    
    The pte entries stil need tagging, but that is not possible in ARCv2
    since the LSB 2 bits are currently used.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/pgalloc.h
+++ arch/arc/include/asm/pgalloc.h
@@ -43,7 +43,7 @@ pmd_populate_kernel(struct mm_struct *mm, pmd_t *pmd, pte_t *pte)
 	 *
 	 * The cast itself is needed given simplistic definition of set_pmd()
 	 */
-	set_pmd(pmd, __pmd((unsigned long)pte));
+	set_pmd(pmd, __pmd((unsigned long)pte | _PAGE_TABLE));
 }
 
 static inline void pmd_populate(struct mm_struct *mm, pmd_t *pmd, pgtable_t pte_page)
@@ -74,7 +74,7 @@ static inline pgd_t *pgd_alloc(struct mm_struct *mm)
 
 static inline void p4d_populate(struct mm_struct *mm, p4d_t *p4dp, pud_t *pudp)
 {
-	set_p4d(p4dp, __p4d((unsigned long)pudp));
+	set_p4d(p4dp, __p4d((unsigned long)pudp | _PAGE_TABLE));
 }
 
 static inline pud_t *pud_alloc_one(struct mm_struct *mm, unsigned long addr)
@@ -96,7 +96,7 @@ static inline void pud_free(struct mm_struct *mm, pud_t *pudp)
 
 static inline void pud_populate(struct mm_struct *mm, pud_t *pudp, pmd_t *pmdp)
 {
-	set_pud(pudp, __pud((unsigned long)pmdp));
+	set_pud(pudp, __pud((unsigned long)pmdp | _PAGE_TABLE));
 }
 
 static inline pmd_t *pmd_alloc_one(struct mm_struct *mm, unsigned long addr)
