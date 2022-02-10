a0b2537d52dcdfd6a909fdbe15671395c822a0ce
Vineet Gupta <vgupta@synopsys.com>
2022-02-10 14:10:08 +0300

    ARC: boot log: eliminate struct cpuinfo_arc #4
     - boot log now clearly per ISA
     - global struct cpuinfo_arc[] elimiated
     - local struct struct arcinfo kept for passing info
       between functions
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/mm/tlb.c
+++ arch/arc/mm/tlb.c
@@ -565,7 +565,7 @@ void local_flush_pmd_tlb_range(struct vm_area_struct *vma, unsigned long start,
  * the cpuinfo structure for later use.
  * No Validation is done here, simply read/convert the BCRs
  */
-char *arc_mmu_mumbojumbo(int c, char *buf, int len)
+int arc_mmu_mumbojumbo(int c, char *buf, int len)
 {
 	struct cpuinfo_arc_mmu *mmu = &mmuinfo;
 	unsigned int bcr, u_dtlb, u_itlb, sasid;
@@ -609,7 +609,7 @@ char *arc_mmu_mumbojumbo(int c, char *buf, int len)
 		       u_dtlb, u_itlb,
 		       IS_AVAIL2(mmu->pae, ", PAE40 ", CONFIG_ARC_HAS_PAE40));
 
-	return buf;
+	return n;
 }
 
 int pae40_exist_but_not_enab(void)
